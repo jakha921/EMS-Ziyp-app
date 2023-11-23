d047597844f41a124381d23364483d23c164bb44
jakha921 <jakha921@gmail.com>
2023-11-24 01:06:09 +0500

    change module router base

--- base/base_service.py
+++ base/base_service.py
@@ -1,7 +1,10 @@
-from fastapi import HTTPException, status
+import math
+
+from fastapi import HTTPException
 from sqlalchemy import select, insert, delete, or_
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import selectinload
+from sqlalchemy.sql.functions import count
 
 from config.db import async_session
 from exeptions import AlreadyExistsException, NotFoundException
@@ -47,11 +50,19 @@ class BaseServices:
         """Получить все model по фильтру"""
         try:
             async with async_session() as session:
-                query = select(cls.model.__table__.columns).filter_by(**kwargs)  # SELECT * FROM model WHERE kwargs
+                # Поиск по фильтру kwargs = {"id": 1, "name": "test"} -> WHERE id=1 AND name="test"
+                kwargs = {key: value for key, value in kwargs.items() if value is not None}
+                query = select(cls.model).filter_by(**kwargs)
+                length_query = select(count(cls.model.id)).filter_by(**kwargs)
 
                 # Поиск по полям
                 if search and cls.search_fields:
                     query = query.where(
+                        or_(
+                            *[getattr(cls.model, field).ilike(f'%{search}%') for field in cls.search_fields]
+                        )
+                    )
+                    length_query = length_query.where(
                         or_(
                             *[getattr(cls.model, field).ilike(f'%{search}%') for field in cls.search_fields]
                         )
@@ -64,24 +75,27 @@ class BaseServices:
 
                 # Пагинация
                 if limit and offset:
-                    query = query.limit(limit).offset(offset)
+                    query = query.offset((offset - 1) * limit).limit(limit)
 
                 # Сортировка
                 query = query.order_by(cls.model.id.desc())
                 result = await session.execute(query)
                 response = result.mappings().all()
 
+                response = [item[f'{(cls.model.__tablename__).capitalize()}'] for item in response]
+
+                # Подсчет количества записей
+                result_count_items = (await session.execute(length_query)).scalar()
+
                 return {
                     "status": "success",
                     "detail": f"Get {cls.model.__tablename__} successfully",
                     "pagination": {
-                        "total": len(response),
-                        "limit": limit,
-                        "offset": offset,
-                        "has_next_page": True if len(response) == limit else False,
-                        "has_previous_page": True if offset > 0 else False,
-                        "current_page": offset + 1 if offset else 1,
-                        "total_pages": len(response) // limit + 1 if len(response) % limit else len(response) // limit
+                        "total_pages": math.ceil(result_count_items / limit),
+                        "current_page": offset,
+                        "total_items": result_count_items,
+                        "has_next_page": True if offset < math.ceil(result_count_items / limit) else False,
+                        "has_previous_page": True if offset > 1 else False,
                     } if limit and offset else None,
                     "data": response
                 }
