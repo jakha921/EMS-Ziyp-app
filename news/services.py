51387af057230cd3028c75bd9a9ab356444f1756
jakha921 <jakha921@gmail.com>
2023-11-26 16:53:58 +0500

    update all modules add filters

--- news/services.py
+++ news/services.py
@@ -4,3 +4,11 @@ from base.base_service import BaseServices
 
 class NewsServices(BaseServices):
     model = News
+    search_fields = [
+        "name_ru",
+        "name_en",
+        "name_uz",
+        "description_ru",
+        "description_en",
+        "description_uz"
+    ]
