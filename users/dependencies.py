dbcc6846d317adfb4a0942ab64fe5513c201a045
jakha921 <jakha921@gmail.com>
2023-11-27 12:19:34 +0500

    add jwt bearer

--- users/dependencies.py
+++ users/dependencies.py
@@ -2,10 +2,25 @@ from datetime import datetime
 
 import jwt
 from fastapi import Request, HTTPException, Depends, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from config.settings import settings
 from exeptions import NotAuthorizedException, NotValidCredentialsException, UserNotFoundException, TokenExpiredException
 from users.services import UserServices
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+            return credentials.credentials
+        else:
+            raise HTTPException(status_code=403, detail="Invalid authorization code.")
 
 
 def get_token(request: Request):
@@ -15,7 +30,8 @@ def get_token(request: Request):
     return token
 
 
-async def get_current_user(token: str = Depends(get_token)):
+async def get_current_user(token: str = Depends(get_token),
+                           credentials: HTTPAuthorizationCredentials = Depends(JWTBearer())):
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         expire: str = payload.get("exp")
