143f77e9b39bbf5176e58dbfada3d39a09bda75e
jakha921 <jakha921@gmail.com>
2024-03-05 13:49:34 +0500

    bug fix push notification

--- notification/firebase_notification.py
+++ notification/firebase_notification.py
@@ -6,16 +6,17 @@ initialize_app(cred)
 
 async def send_push_notification(token: str, title: str, body: str):
     try:
-        message = messaging.Message(
-            notification=messaging.Notification(
-                title=title,
-                body=body,
-            ),
-            token=token,
-        )
+        if token:
+            message = messaging.Message(
+                notification=messaging.Notification(
+                    title=title,
+                    body=body,
+                ),
+                token=token,
+            )
 
-        response = messaging.send(message)
-        return {"response": response}
+            response = messaging.send(message)
+            return {"response": response}
     except exceptions.InvalidArgumentError as e:
         # Handle the error here
         print("Caught InvalidArgumentError:", e)
